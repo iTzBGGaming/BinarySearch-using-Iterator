dbe2db008969b1bd7ff30eabb228cc79a0441998
iTzBGGaming <45941903+iTzBGGaming@users.noreply.github.com>
2022-11-21 17:30:58 +0800

    Add files via upload

---
+++ LinkyListy.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkyListy {
+	
+	public static void main(String []args) {
+		LinkedList linky = new LinkedList();
+		Scanner in = new Scanner(System.in);
+		
+		
+		//Add something in LinkedList
+		linky.add("Pisang");
+		linky.add("Rambutan");
+		linky.add("Durian");
+		linky.add("Cempaka");
+		linky.add(0,"Nasi"); //add an element to linked list at index that you desired
+		
+		//if you dont know which element in index in linked list you're looking for. aka BINARY SEARCH
+		Iterator it = linky.iterator();
+			System.out.println("********** INDEX SEARCHER **********");
+			System.out.println("Search for a food? (Pisang,Rambutan,Durian,Cempaka,Nasi)");
+			String choice = in.nextLine();
+
+			while(it.hasNext()) {
+				if(((String)it.next()).equalsIgnoreCase(choice)) {
+					System.out.println("it's in index "+linky.indexOf(it.next()));
+				}
+			}	
+	}
+}
